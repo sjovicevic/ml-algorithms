9af3fe4f1732958a892e0db292e38f26d6043a29
Mac Rezerva <intern@Macs-MBP.localdomain>
2023-05-29 15:39:46 +0200

    initial create

---
+++ train.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from LogisticRegression import LogisticRegression
+
+def accuracy(y_pred, y_test):
+    return np.sum(y_pred == y_test) / len(y_test)
+
+bc = datasets.load_breast_cancer()
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+print(iris.data[:3])
+
+targets = set(iris.target)
+print(targets)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+plt.plot(X_train, y_train, c='r')
+plt.show()
+
+clf = LogisticRegression(alpha=0.1)
+clf.fit(X_test, y_test)
+y_pred = clf.predict(X_test)
+
+acc = accuracy(y_pred, y_test)
+print(acc)
