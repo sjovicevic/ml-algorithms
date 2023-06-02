962f541a596e746d3a5937c8e5d09928efe8c635
Stefan Jovicevic <jovicevicst@gmail.com>
2023-06-02 13:38:27 +0200

    Prepravke pre novog feature-a

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
-def sigmoid(z):
+def compute_sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+
+def compute_softmax(func_value, samples):
+    return np.exp(func_value) / np.sum(np.exp(func_value), axis=1).reshape(samples, 1)
 
 
 class LogisticRegression:
@@ -24,11 +28,10 @@ class LogisticRegression:
 
     def binary_classification(self, X, y):
         n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        self.weights = np.zeros(n_features + 1)
         for _ in range(self.n_iters):
             lin_prediction = np.dot(X, self.weights) + self.bias
-            predictions = sigmoid(lin_prediction)
+            predictions = compute_sigmoid(lin_prediction)
 
             dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
             db = (1 / n_samples) * np.sum(predictions - y)
@@ -41,20 +44,14 @@ class LogisticRegression:
         y_one_hot = np.array(pd.get_dummies(y, dtype='int8'))
         n_classes = len(y_one_hot[0])
 
-        print(X)
-
-        self.weights = np.random.uniform(low=-1, high=1, size=(n_classes, n_features)).T
+        self.weights = np.random.uniform(low=-1, high=1, size=(n_classes, n_features))
+        self.weights = self.weights.T
         self.bias = 0
-
-        print(f"X shape: {X.shape}")
-        print(f"Y shape: {y.shape}")
-        print(f"Y one hot shape: {y_one_hot.shape}")
-        print(f"Weights shape: {self.weights.shape}")
 
         for _ in range(self.n_iters):
             f_wb = np.dot(X, self.weights) + self.bias
 
-            softmax = np.exp(f_wb) / np.sum(np.exp(f_wb), axis=1).reshape(n_samples, 1)
+            softmax = compute_softmax(f_wb, n_samples)
 
             loss = (-1 / n_samples) * (np.sum(np.multiply(y_one_hot, np.log(softmax))))
 
@@ -77,5 +74,4 @@ class LogisticRegression:
         f_wb = np.dot(X, self.weights) + self.bias
         softmax = np.exp(f_wb) / np.sum(np.exp(f_wb), axis=1).reshape(n_samples, 1)
         indices = np.argmax(softmax, axis=1)
-        print(indices)
         return indices
