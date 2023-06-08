1cbe80dadc07c5f34aa702c8a2a0fa1258a342bd
Stefan Jovicevic <jovicevicst@gmail.com>
2023-06-08 10:28:01 +0200

    Code refactorization, just a little bit

--- neural_network.py
+++ neural_network.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nnfs
 from nnfs.datasets import spiral_data
-from logistic_regression import compute_sigmoid
 
 
 class LayerDense:
@@ -10,6 +9,7 @@ class LayerDense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.output = None
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -17,16 +17,20 @@ class LayerDense:
 
 class ActivationReLU:
 
+    def __init__(self):
+        self.output = None
+
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
 
 class ActivationSoftmax:
 
+    def __init__(self):
+        self.output = None
+
     def forward(self, inputs):
-
         exponents = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-
         self.output = exponents / np.sum(exponents, axis=1, keepdims=True)
 
 
