e8b0db442377389586f3c7837d2a097bf87f503d
Stefan Jovicevic <jovicevicst@gmail.com>
2023-06-19 14:36:22 +0200

    added testing file

--- neural_network_iris.py
+++ neural_network_iris.py
@@ -6,7 +6,7 @@ import utils
 
 class Layer:
 
-    def __init__(self, input, n_neurons, activation_f):
+    def __init__(self, input, output, n_neurons, activation_f):
         self.input = input
         self.n_samples = self.input.shape[0]
         self.n_features = self.input.shape[1]
@@ -15,9 +15,18 @@ class Layer:
         self.cache = {}
         self.activation_f = activation_f
         self.grads = None
+        self.output = output
+        self.weights = None
+        self.y_one_hot = None
+        self.expected_value = None
 
-        self.weights = np.random.uniform(low=-1, high=1, size=(self.n_features, self.n_neurons))
+        #self.weights = np.random.uniform(low=-1, high=1, size=(self.n_features, self.n_neurons))
         self.bias = np.zeros(self.n_neurons)
+
+    def initialize(self):
+        self.y_one_hot = np.array(pd.get_dummies(y, dtype='int8'))
+        n_classes = len(self.y_one_hot[0])
+        self.weights = np.random.uniform(low=-1, high=1, size=(self.n_features, n_classes))
 
     def forward(self):
         self.cache['Z'] = np.dot(self.input, self.weights) + self.bias
@@ -30,8 +39,6 @@ class Layer:
         pass
 
     def back(self, expected, output):
-        output_one_hot = np.array(pd.get_dummies(output, dtype='int8'))
-
         dz = output - expected.T
         self.weights = (1 / self.n_samples) * np.matmul(dz, self.cache['Activation'].T)
         self.bias = (1 / self.n_samples) * np.sum(dz, axis=1, keepdims=True)
@@ -42,5 +49,6 @@ multiclass, X, y = ldr.run()
 print(f'X values: {X.shape}')
 print(f'Y values: {y.shape}')
 
-layer = Layer(X, 3, activation_f=utils.softmax)
-layer.forward()
+layer = Layer(X, y, 1, activation_f=utils.softmax)
+layer.initialize()
+print(layer.forward())
