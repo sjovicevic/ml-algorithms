935656a11494817656a017f0f00947910e930455
Stefan Jovicevic <jovicevicst@gmail.com>
2023-06-28 12:04:38 +0200

    wrote loading functions for layers

--- src/neural_network.py
+++ src/neural_network.py
@@ -9,34 +9,31 @@ import utils
 class Layer:
 
     def __init__(self,
-                 data,
                  n_neurons,
                  activation_f,
-                 derivative_f,
-                 output_neuron=False,
-                 y=None):
-
-        self.input = data
+                 output_layer=None):
         self.n_neurons = n_neurons
         self.activation_f = activation_f
-        self.derivative_f = derivative_f
-        self.output_neuron = output_neuron
+        self.derivative_f = f'utils.{activation_f}_derivative'
+        self.output_layer = output_layer
         self.y = y
-        self.n_features = data.shape[1]
         self.memory = {}
         self.bias = 0
         self.weights = np.random.uniform(low=-1, high=1, size=(self.n_features, self.n_neurons))
 
-        if y is not None and output_neuron:
+        if y is not None and output_layer:
             y_one_hot = utils.get_one_hot(y)
             if len(y_one_hot[0]) != n_neurons:
                 raise Exception("Number of neurons must be equal to the number of the output classes.")
 
-    def forward(self, data, y=None):
-        self.memory['Z'] = np.dot(self.input, self.weights) + self.bias
+    def initialize_parameters(self, input):
+        self.weights = np.random.uniform(low=-1, high=1, size=(input.shape[1], self.n_neurons))
+
+    def forward(self, input, y=None):
+        self.memory['Z'] = np.dot(input, self.weights) + self.bias
         self.memory['Activation'] = self.activation_f(self.memory['Z'])
 
-        if self.output_neuron:
+        if self.output_layer:
             loss = utils.loss(self.memory['Activation'], y_train, self.input.shape[0])
             return loss
         else:
@@ -44,7 +41,7 @@ class Layer:
 
     def backward(self, next_activation=None, next_weights=None, previous_derivative=None):
 
-        if self.output_neuron:
+        if self.output_layer:
             self.y = np.array(pd.get_dummies(self.y, dtype='int8'))
             self.memory['Loss_derivative_value'] = self.derivative_f(self.y, self.memory['Activation'])
             return self.memory['Loss_derivative_value']
@@ -70,19 +67,27 @@ class NeuralNetwork:
         self.layers = []
 
     def load(self, layer):
-        self.layers.append(self.load_input_layer(layer['input']))
-        self.layers.append(self.load_hidden_layer(layer['hidden']))
-        self.layers.append(self.load_output_layer(layer['output']))
 
-    def load_input_layer(self, layer_info: list):
-        pass
-        # return Layer(layer_info[0], layer_info[1])
+        """
+        This function loads layers into neural network.
+        :param layer: dictionary with keywords 'input', 'hidden', 'output' with list with two parameters as values.
+        First parameter represents number of neurons, second parameter represents activation function.
+        :return: List with Layer objects [Layer(input), Layer(hidden)..., Layer(output)]
+        """
+        _layers = [self.load_layer(layer['input'])]
+        for layer_info in layer['hidden']:
+            _layers.append(self.load_layer(layer_info))
+        _layers.append(self.load_output_layer(layer['output']))
 
-    def load_hidden_layer(self, layer_info: list):
-        pass
+        return _layers
 
-    def load_output_layer(self, layer_info: list):
-        pass
+    @staticmethod
+    def load_layer(layer_info: list):
+        return Layer(layer_info[0], layer_info[1])
+
+    @staticmethod
+    def load_output_layer(layer_info: list):
+        return Layer(layer_info[0], layer_info[1], output_layer=True)
 
     def train(self):
         pass
@@ -98,6 +103,3 @@ layers = {
     'output': [3, utils.softmax]
 }
 
-
-for parameter, index in zip(params, range(len(params))):
-    pass
