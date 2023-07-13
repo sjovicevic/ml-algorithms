3acd2f42e5421bea94bf41ddb42d87f61e8783fb
Stefan Jovicevic <jovicevicst@gmail.com>
2023-07-13 15:07:21 +0200

    model performance is good now

--- src/neural_network.py
+++ src/neural_network.py
@@ -154,7 +154,7 @@ class NeuralNetwork:
 ldr = utils.DatasetLoader(dataset=datasets.load_iris(), multiclass_flag=True)
 multiclass, X, y = ldr.run()
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=1234)
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.125, random_state=1234)
 
 layers_in = [
             {'input': 4, 'output': 128, 'activation_f': utils.tanh},
